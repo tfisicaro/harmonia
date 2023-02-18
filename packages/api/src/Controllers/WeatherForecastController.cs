aaec3435329015e060ccc1a8d62740b0f779e26a
Tim Fisicaro <tim.fisicaro@xenial.cloud>
2023-02-18 21:41:55 +0100

    feat: :sparkles: read openapi info from appsettings
    Took 22 minutes

--- packages/api/src/Controllers/WeatherForecastController.cs
+++ packages/api/src/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
@@ -25,7 +25,7 @@ public class WeatherForecastController : ControllerBase
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
             })
             .ToArray();
     }
--- packages/api/src/Program.cs
+++ packages/api/src/Program.cs
@@ -1,11 +1,11 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.SwaggerDoc("v1", builder.Configuration.GetSection("OpenApiSettings").Get<OpenApiInfo>()));
 
 var app = builder.Build();
 
@@ -17,9 +17,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
